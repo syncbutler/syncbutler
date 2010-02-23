ac210427f85d414e19ff265809fce4a4cbffb7ae
bryanthomaschen <bryanthomaschen@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-02-23 20:06:46 +0000

    Added new namespace SyncButler.SystemEnvironment Added new class StorageDevices: contains methods to query and identify storage devices' properties

---
+++ syncbutler/SystemEnvironment/StorageDevices.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+namespace SyncButler.SystemEnvironment
+{
+    public class StorageDevices
+    {
+        public static String GetDriveLetter(String driveID)
+        {
+            String letter = "";
+            ManagementObjectSearcher DDMgmtObjSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            
+            foreach (ManagementObject DDObj in DDMgmtObjSearcher.Get())
+            {
+                if (DDObj["PNPDeviceID"].ToString().Equals(driveID))
+                {
+                    foreach (ManagementObject DPObj in DDObj.GetRelated("Win32_DiskPartition"))
+                    {
+                        foreach (ManagementObject LDObj in DPObj.GetRelated("Win32_LogicalDisk"))
+                        {
+                            letter = LDObj["DeviceID"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return letter;
+        }
+
+        public static String GetDriveID(String driveLetter)
+        {
+            String id = "";
+            ManagementObjectSearcher DDMgmtObjSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk WHERE DeviceID='" + driveLetter.TrimEnd('\\') + "'");
+
+            foreach (ManagementObject DDObj in DDMgmtObjSearcher.Get())
+            {
+                foreach (ManagementObject DPObj in DDObj.GetRelated("Win32_DiskPartition"))
+                {
+                    foreach (ManagementObject LDObj in DPObj.GetRelated("Win32_DiskDrive"))
+                    {
+                        id = LDObj["PNPDeviceID"].ToString();
+                    }
+                }
+            }
+
+            return id;
+        }
+    }
+}
