03c32b3f45d47d2dd35376d9dd03f10c314699f1
pws1906 <pws1906@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-02-26 07:49:34 +0000

    modified the following methods in MostRecentlyUsedFile   - Get();   - GetNonPidl();   - GetPidl();
    added the methods in MostRecentlyUsedFile to get a list of MRU

--- syncbutler/MostRecentlyUsedFile.cs
+++ syncbutler/MostRecentlyUsedFile.cs
@@ -32,21 +32,84 @@ namespace SyncButler
         public static string Get(string key)
         {
             if (Environment.OSVersion.Version.Major >= 6)
-                return GetPidl(key);
+                return GetPidl(key, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSavePidlMRU\\*");
             else
-                return GetNonPidl(key);
+                return GetNonPidl(key, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSaveMRU\\*");
         }
+        
+        /// <summary>
+        /// Get the most recently used (MRU) file as a sorted liist
+        /// </summary>
+        /// <param name="key">The key of the MRU</param>
+        /// <returns>return path of the file</returns>
+        public static SortedList<string,string> Get()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                return GetPidl("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSavePidlMRU\\*");
+            else
+                return GetNonPidl("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSaveMRU\\*");
+        }
+
+        /// <summary>
+        /// Get a sorted list of most recently used (MRU) files for windows xp and below
+        /// </summary>
+        /// <param name="key">Registry key of the MRU</param>
+        /// <returns>A sorted list of the MRU</returns>
+        /// <exception cref="System.NullReferenceException">The given registry key is not valid</exception>
+        private static SortedList<string, string> GetNonPidl(string key)
+        {
+            SortedList<string, string> mrus = new SortedList<string, string>();
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(key);
+            if(regKey == null)
+                throw new NullReferenceException();
+            foreach (string index in regKey.GetValueNames())
+            {
+                if (!index.Equals("MRUListEx"))
+                {
+                    mrus.Add(index, (string)regKey.GetValue(index));
+                }
+            }
+
+            return mrus;
+            
+        }
+
+        /// <summary>
+        /// Get a sorted list of most recently used (MRU) files for windows vista and above
+        /// </summary>
+        /// <param name="key">Registry key of the MRU</param>
+        /// <returns>A sorted list of the MRU</returns>
+        /// <exception cref="System.NullReferenceException">The given registry key is not valid</exception>
+        private static SortedList<string, string> GetPidl(string key)
+        {
+            SortedList<string, string> mrus = new SortedList<string, string>();
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(key);
+            if (regKey == null)
+                throw new NullReferenceException();
+            foreach (string index in regKey.GetValueNames())
+            {
+                if (!index.Equals("MRUListEx"))
+                {
+                    mrus.Add(index, GetPidl(index, key));
+                }
+            }
+            return mrus;
+        }
+
 
         /// <summary>
         /// Get the most recently used (MRU) file for windows xp and below
         /// </summary>
-        /// <param name="key">The key of the MRU</param>
+        /// <param name="index">The index of the MRU</param>
+        /// <param name="key">The registry key of the MRU</param>
         /// <returns>return path of the file</returns>
-        /// <exception cref="System.NullReferenceException">The given key is not found.</exception>
-        private static string GetNonPidl(string key)
+        /// <exception cref="System.NullReferenceException">The given registry key or the index is not found.</exception>
+        private static string GetNonPidl(string index, string key)
         {
-            RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSaveMRU\\*");
-            string value = (string)regKey.GetValue(key);
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(key);
+            if (regKey == null)
+                throw new NullReferenceException();
+            string value = (string)regKey.GetValue(index);
             if (value == null)
                 throw new NullReferenceException();
             return value;
@@ -55,13 +118,18 @@ namespace SyncButler
         /// <summary>
         /// Get the most recently used (MRU) file for windows vista and above
         /// </summary>
-        /// <param name="key">The key of the MRU</param>
+        /// <param name="index">The index of the MRU</param>
+        /// <param name="key">The registry key to the MRU</param>
         /// <returns>return path of the file</returns>
-        /// <exception cref="System.NullReferenceException">The given key is not found.</exception>
-        private static string GetPidl(string key)
+        /// <exception cref="System.NullReferenceException">The given registry key or the index is not found.</exception>
+        private static string GetPidl(string index, string key)
         {
-            RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSavePidlMRU\\*");
-            object value = regKey.GetValue(key);
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(key);
+
+            if (regKey == null)
+                throw new NullReferenceException();
+
+            object value = regKey.GetValue(index);
 
             if (value == null)
                 throw new NullReferenceException();
