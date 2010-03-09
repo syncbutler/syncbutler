97943fe1f6907561a2330437bf7b79e01e205972
bloodyangel.rachel <bloodyangel.rachel@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-03-09 13:52:17 +0000

    application icon is changed to sync butler custom dialog box partially done. flexible: handles question,error type messages. Sync all now checks for confirmation and if partnerships exists. added syncbutler with error image for dialogbox

--- trunk/syncbutler-ui/SyncButlerUI/CustomDialog.xaml.cs
+++ trunk/syncbutler-ui/SyncButlerUI/CustomDialog.xaml.cs
@@ -17,15 +17,78 @@ namespace SyncButlerUI
 	/// </summary>
 	public partial class CustomDialog : Window
 	{
-        
+		public enum MessageType
+		{
+			Error,
+			Success,
+			Message,
+			Warning,
+			Question
+		}
+        //default msg box
 		public CustomDialog(string msg){
 			InitializeComponent();
-			SyncButlerMessage.Text=msg;
+			syncButlerMessage.Text=msg;
 		}
 		
-		private void OkClick(object sender, RoutedEventArgs e)		
+		public CustomDialog(MessageType msgType, string msg){
+			InitializeComponent();
+			messageTypetoImage(msgType,msg);
+
+		}
+		
+		private void messageTypetoImage(MessageType msgType,string msg){
+			string imageUri="Images/logowTransparency.png";
+			Visibility yesButtonVisible=System.Windows.Visibility.Hidden;
+			Visibility noButtonVisible=System.Windows.Visibility.Hidden;
+			Visibility okButtonVisible=System.Windows.Visibility.Hidden;
+			string dialogTitle="";
+			switch(msgType){
+				case MessageType.Error:
+					okButtonVisible=System.Windows.Visibility.Visible;
+					dialogTitle="Error";
+					imageUri="Images/error.png";
+					break;
+				case MessageType.Success:
+					okButtonVisible=System.Windows.Visibility.Visible;
+					dialogTitle="Success";
+					break;
+				case MessageType.Message:
+					imageUri="Images/logowTransparency.png";
+					okButtonVisible=System.Windows.Visibility.Visible;
+					dialogTitle="Message";
+					break;
+				case MessageType.Warning:
+					okButtonVisible=System.Windows.Visibility.Visible;
+					dialogTitle="Warning";
+					break;
+				case MessageType.Question:
+					yesButtonVisible=System.Windows.Visibility.Visible;
+					noButtonVisible=System.Windows.Visibility.Visible;
+					break;	
+				default:	
+				    break;	
+			}
+			Uri src = new Uri(@imageUri, UriKind.Relative);
+ 			
+			BitmapImage img = new BitmapImage(src);
+ 			syncButlerMessage.Text=msg;
+			messageImage.Source=img;
+			yesButton.Visibility=yesButtonVisible;
+			noButton.Visibility=noButtonVisible;
+			okButton.Visibility=okButtonVisible;
+			this.Title=dialogTitle;
+		
+		}
+		
+		
+		private void yesClick(object sender, RoutedEventArgs e)		
 		{
 			this.DialogResult = true;		
+		}
+		private void noClick(object sender, RoutedEventArgs e)		
+		{
+			this.DialogResult = false;		
 		}
 
 	}
