a9b5f5182c422e9b1c400f35543e375293acd027
bryanthomaschen <bryanthomaschen@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-03-15 08:39:46 +0000

    Added methods to enable retrieval of drive format and available space.

---
+++ syncbutler/Exceptions/UnknownStorageFormatException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyncButler.Exceptions
+{
+    /// <summary>
+    /// Thrown when the format of the file system is unknown.
+    /// </summary>
+    public class UnknownStorageFormatException : Exception
+    {
+    }
+}
