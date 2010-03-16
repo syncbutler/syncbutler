c71bd5d97a907dd4f5c4188f27b8851e085f4b8a
tance.e6p3f2 <tance.e6p3f2@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-03-16 12:03:40 +0000

    Included missing exception file.

---
+++ trunk/syncbutler/Exceptions/UserCancelledException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyncButler.Exceptions
+{
+    public class UserCancelledException : Exception
+    {
+    }
+}
