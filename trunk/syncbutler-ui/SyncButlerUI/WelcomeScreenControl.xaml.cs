863e7b2ddbf79bfc38e78638c10aae255c748666
pws1906 <pws1906@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-03-27 08:39:31 +0000

    blocked some computer name that is a reserved keyword

--- trunk/syncbutler-ui/SyncButlerUI/WelcomeScreenControl.xaml.cs
+++ trunk/syncbutler-ui/SyncButlerUI/WelcomeScreenControl.xaml.cs
@@ -19,6 +19,7 @@ namespace SyncButlerUI
 	/// </summary>
 	public partial class WelcomeScreenControl : UserControl
 	{
+        private static string[] reserved = { "con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8" };
 		public Controller controller;
 		public WelcomeScreenControl()
 		{
@@ -27,13 +28,17 @@ namespace SyncButlerUI
 		
 		public void NameIt_Click(object sender,RoutedEventArgs e)
         {
-
-            if (FirstTimeComputerNameText.Text.Length != 0)
+            if (Array.IndexOf(reserved, FirstTimeComputerNameText.Text.Trim()) != -1)
+            {
+                CustomDialog.Show(this, CustomDialog.MessageTemplate.OkOnly, CustomDialog.MessageResponse.Ok, FirstTimeComputerNameText.Text.Trim() + " is not a valid name");
+            }
+            else if (FirstTimeComputerNameText.Text.Length != 0)
             {
                 controller = Controller.GetInstance();
-                controller.SetFirstComputerName(FirstTimeComputerNameText.Text);
-				VisualStateManager.GoToState(this,"HelpScreen1",false);
-			}
+                controller.SetFirstComputerName(FirstTimeComputerNameText.Text.Trim());
+
+                VisualStateManager.GoToState(this, "HelpScreen1", false);
+            }
             else
             {
                 CustomDialog.Show(this, CustomDialog.MessageTemplate.OkOnly, CustomDialog.MessageResponse.Ok, "Please enter a computer name");
