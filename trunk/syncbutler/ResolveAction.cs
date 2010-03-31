052b387b8dc0a0ccc4a85b0b0c6f27434675bd3b
bryanthomaschen <bryanthomaschen@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-03-31 12:30:15 +0000

    Extracted ResolveAction and ResolveActionSet to different physical files. Modified some properties and methods and encapsulated some variables.

---
+++ trunk/syncbutler/ResolveAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyncButler
+{
+    /// <summary>
+    /// Represents an action to take and its text description.
+    /// </summary>
+    public class ResolveAction
+    {
+
+        /// <summary>
+        /// Gets or sets the action to use when resolving a conflict.
+        /// </summary>
+        public Conflict.Action ResolutionAction { get; set;}
+
+        /// <summary>
+        /// Gets or sets the description of the action to be taken.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="resolutionAction">The action to use when resolving a conflict.</param>
+        /// <param name="description">The description of the action to be taken.</param>
+        public ResolveAction(Conflict.Action resolutionAction, string description)
+        {
+            this.ResolutionAction = resolutionAction;
+            this.Description = description;
+        }
+    }
+}
---
+++ trunk/syncbutler/ResolveActionSet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SyncButler
+{
+    /// <summary>
+    /// Represents a list of actions that can be taken and the selected action to take.
+    /// </summary>
+    public class ResolveActionSet : ObservableCollection<ResolveAction>
+    {
+        private ResolveAction selectedAction;
+
+        /// <summary>
+        /// Gets or sets the action selected by the user.
+        /// </summary>
+        public ResolveAction SelectedAction
+        {
+            get
+            {
+                return this.selectedAction;
+            }
+        }
+
+        /// <summary>
+        /// Add an action to this set.
+        /// </summary>
+        /// <param name="toAdd">The action to add.</param>
+        public void AddAction(Conflict.Action toAdd)
+        {
+            this.Add(new ResolveAction(toAdd, toAdd.ToString()));
+        }
+
+        /// <summary>
+        /// Sets the action selected by the user.
+        /// </summary>
+        /// <param name="toSet">The selected action.</param>
+        public void SetSelectedAction(Conflict.Action toSet)
+        {
+            this.selectedAction = null;
+            foreach (ResolveAction action in this)
+            {
+                if (action.ResolutionAction == toSet)
+                {
+                    this.selectedAction = action;
+                    break;
+                }
+            }
+        }
+    }
+}
