9890290ac3ac8056b7985243e0e206cb820f4cf1
pws1906 <pws1906@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-04-07 06:59:13 +0000

    partial fix on issue 117 Added WindowDriveInfo to represent drive information (i.e. drive letter, labels) to the user

---
+++ syncbutler/WindowDriveInfo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace SyncButler
+{
+    /// <summary>
+    /// A logical repesentation of drive information
+    /// </summary>
+    class WindowDriveInfo
+    {
+        private char DriveLetter;
+        private String Label;
+
+        /// <summary>
+        /// The constructor to initilize the class
+        /// </summary>
+        /// <param name="DriveLetter">Create a drive info base on the drive letter</param>
+        /// <exception cref="System.Exception">If the given drive letter is not a valid drive</exception>
+        public WindowDriveInfo(String DriveLetter)
+        {
+            if (DriveLetter.Length == 0)
+            {
+                throw new Exception("invalid drive");
+            }
+            this.DriveLetter = DriveLetter[0];
+
+            if (!Directory.Exists(DriveLetter + ":\\"))
+            {
+                throw new Exception("invalid drive");
+            }
+
+            DriveInfo di = new DriveInfo(DriveLetter);
+            Label = di.VolumeLabel;
+        }
+
+        /// <summary>
+        /// The constructor to initilize the class
+        /// </summary>
+        /// <param name="DriveLetter">Create a drive info base on the drive letter</param>
+        /// <exception cref="System.Exception">If the given drive letter is not a valid drive</exception>
+        public WindowDriveInfo(char DriveLetter)
+        {
+            this.DriveLetter = DriveLetter;
+
+            if (!Directory.Exists(DriveLetter + ":\\"))
+            {
+                throw new Exception("invalid drive");
+            }
+
+            DriveInfo di = new DriveInfo("" + DriveLetter);
+            Label = di.VolumeLabel;
+        }
+
+        /// <summary>
+        /// Get a list of drive information base on a list of drive letters
+        /// </summary>
+        /// <param name="DriveLetters">A list of drive letters</param>
+        /// <returns>A list of drive information</returns>
+        public static List<WindowDriveInfo> GetDriveInfo(Collection<String> DriveLetters)
+        {
+            List<WindowDriveInfo> ToRtn = new List<WindowDriveInfo>(); 
+            foreach (String s in DriveLetters)
+            {
+                ToRtn.Add(new WindowDriveInfo(s));
+            }
+            return ToRtn;
+        }
+
+
+        public override string ToString()
+        {
+            return Label + " - " + "[" + DriveLetter + ":]";
+        }
+    }
+}
