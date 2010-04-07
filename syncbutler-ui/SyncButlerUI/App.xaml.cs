913dab51c3f5d81cbe00e3027cec91b13696672a
pws1906 <pws1906@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-04-07 10:50:59 +0000

    fixed issue 114 the program will now let the user know if some .dll files are mising.

--- syncbutler-ui/SyncButlerUI/App.xaml.cs
+++ syncbutler-ui/SyncButlerUI/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Threading;
 using SyncButler;
 using SyncButler.Exceptions;
+using System.IO;
 
 namespace SyncButlerUI
 {
@@ -15,38 +16,59 @@ namespace SyncButlerUI
 	/// </summary>
 	public partial class App : Application
 	{
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve); 
+            SyncButlerUI.App app = new SyncButlerUI.App();
+            app.InitializeComponent();
+            app.Run();           
+        }
+        /// <summary>
+        ///  This method will be fire if some assembly is missing, can be used to spawn the missing files, if required.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            MessageBox.Show("Some important files are missing\r\nSBS will not run.");
+            Environment.Exit(-1);
+            return null;
+        } 
         /// <summary>
         /// Overrides the default OnStartup to provide for testing of single instance.
         /// </summary>
         /// <param name="e">Contains arguments from the event; used to access command line parameters.</param>
         protected override void OnStartup(StartupEventArgs e)
         {
-            if (Controller.IsOnCDRom())
-            {
-                MessageBox.Show("Running SBS on CD Rom is Not Supported", "Not Supported", MessageBoxButton.OK, MessageBoxImage.Information);
-                Environment.Exit(-1);
-            }
-            else
-            {
-                if (Controller.TestSingleInstance(e.Args))
+                if (Controller.IsOnCDRom())
                 {
-                    try
+                    MessageBox.Show("Running SBS on CD Rom is Not Supported", "Not Supported", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Environment.Exit(-1);
+                }
+                else
+                {
+                    if (Controller.TestSingleInstance(e.Args))
                     {
-                        new MainWindow().ShowDialog();
+                        try
+                        {
+                            new MainWindow().ShowDialog();
+                        }
+                        catch (UserCancelledException)
+                        {
+                            base.Shutdown(0);
+                        }
+
                     }
-                    catch (UserCancelledException)
+                    else
                     {
                         base.Shutdown(0);
                     }
 
+                    base.OnStartup(e);
                 }
-                else
-                {
-                    base.Shutdown(0);
-                }
-            
-                base.OnStartup(e);
-            }
         }
 	}
 }
