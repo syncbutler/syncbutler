6d99e2157acc0df6b74cb11c74a98db80d3bac85
bryanthomaschen <bryanthomaschen@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-04-10 16:01:19 +0000

    Added a new project for 'uninstalling' Sync Butler. Read UninstallUtils.cs in package SyncButler.Uninstaller for more details.

---
+++ syncbutler-uninstaller/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SyncButler.Uninstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(560, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registry keys that will be removed (under HKEY_CURRENT_USER)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Files that will be removed";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 157);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox2.Size = new System.Drawing.Size(558, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Files that will be kept";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 287);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox3.Size = new System.Drawing.Size(558, 95);
+            this.listBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(558, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Uninstall!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 447);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sync Butler Uninstaller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ syncbutler-uninstaller/UninstallUtils.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+
+namespace SyncButler.Uninstaller
+{
+    /// <summary>
+    /// UninstallUtils provides the basic framework for cleaning up stuff left by Sync Butler.
+    /// </summary>
+    public class UninstallUtils
+    {
+        /* Registry keys that should be deleted (in proper order) */
+        public static string[] DELETE_REG_LIST = {
+            @"Software\Classes\AllFilesystemObjects\shell\Sync Butler, Sync!",
+        };
+
+        /* Files that should remain (file names only, no full paths) */
+        public static string[] DELETE_FILE_LIST = {
+            @"log.xml",
+            @"logstyle.css",
+            @"*.butler"
+        };
+
+        /* Files that should always remain even if they match an entry in the DELETE_FILE_LIST */
+        public static string[] KEEP_FILE_LIST = {
+            @"Microsoft.Expression.Interactions.dll",
+            @"SyncButler.dll",
+            @"SyncButler.exe",
+            @"System.Windows.Interactivity.dll",
+            @"WPFToolkit.dll"
+        };
+
+        /// <summary>
+        /// Performs an uninstall.
+        /// </summary>
+        /// <returns>An array containing messages/issues encountered during the sync - typically any exceptions that were caught.</returns>
+        public static string[] Uninstall()
+        {
+            List<string> issues = new List<string>();
+
+            foreach (string key in DELETE_REG_LIST)
+            {
+                try
+                {
+                    RemoveRegistryKey(key);
+                }
+                catch (Exception e)
+                {
+                    issues.Add("Exception occurred while removing registry key (" + key + "): " + e.GetType().Name + ": " + e.Message);
+                }
+            }
+
+            foreach (string file in DELETE_FILE_LIST)
+            {
+
+                if (file.Contains("*"))
+                {
+                    string[] results = Directory.GetFiles(GetRunningDirectory(), file);
+
+                    foreach (string result in results)
+                    {
+                        try
+                        {
+                            if (!IsFileAllowed(result))
+                                File.Delete(result);
+                        }
+                        catch (Exception e)
+                        {
+                            issues.Add("Exception occurred while deleting file (" + file + "): " + e.GetType().Name + ": " + e.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        if (!IsFileAllowed(file))
+                            RemoveFile(file);
+                    }
+                    catch (Exception e)
+                    {
+                        issues.Add("Exception occurred while deleting file (" + file + "): " + e.GetType().Name + ": " + e.Message);
+                    }
+                }
+            }
+
+            return issues.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the directory that the uninstaller is running from.
+        /// </summary>
+        /// <returns>A string of the directory that this uninstaller is running from.</returns>
+        public static string GetRunningDirectory()
+        {
+            string currDir = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            int lastSlashIndex = currDir.LastIndexOf('\\');
+            currDir = currDir.Substring(0, lastSlashIndex);
+
+            return currDir;
+        }
+
+        /// <summary>
+        /// Gets the name of the current executable.
+        /// </summary>
+        /// <returns>The string of the name of this executable.</returns>
+        public static string GetCurrentExeName()
+        {
+            string currExe = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            int lastSlashIndex = currExe.LastIndexOf('\\');
+            currExe = currExe.Substring(lastSlashIndex + 1);
+
+            return currExe;
+        }
+
+        /// <summary>
+        /// Checks whether a file is in the keep list.
+        /// </summary>
+        /// <param name="file">Path to the file you wish to check.</param>
+        /// <returns>True if file is to be kept, false otherwise.</returns>
+        public static bool IsFileAllowed(string file)
+        {
+            bool allowed = false;
+
+            foreach (string entry in KEEP_FILE_LIST)
+            {
+                if (entry.EndsWith(file) || entry.Equals(file))
+                {
+                    allowed = true;
+                    break;
+                }
+            }
+
+            return allowed;
+        }
+
+        /// <summary>
+        /// Gets the list of immediate subkeys within a given key of the registry.
+        /// </summary>
+        /// <param name="path">The parent key.</param>
+        /// <returns>Array containing the names of the subkeys under the parent key provided.</returns>
+        public static string[] GetRegistrySubKeys(string path)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(path);
+            return key.GetSubKeyNames();
+        }
+
+        /// <summary>
+        /// Deletes a registry key and all its subkeys in a recursive manner.
+        /// </summary>
+        /// <param name="path">The key to delete.</param>
+        public static void RemoveRegistryKey(string path)
+        {
+            Registry.CurrentUser.DeleteSubKeyTree(path);
+        }
+
+        /// <summary>
+        /// Deletes a file at the specified path.
+        /// </summary>
+        /// <param name="path">Path of the file to delete.</param>
+        public static void RemoveFile(string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+    }
+}
