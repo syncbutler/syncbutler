b8fc121111b7af18a27117bb4d6ee2365201f8d7
u0707198 <u0707198@e31dacda-1c52-11df-9fc2-77fecae625a6>
2010-04-12 08:45:11 +0000

    Fixed some startup issues. GrabFocus now supports changing to mini partnership page. ErrorList added to display a list of errors at the end of the mini syncs if there exists any. Sync Butler, Sync! changed to Mini-Sync This!; old entry kept in uninstaller to provide removal for older entries.

---
+++ trunk/syncbutler-ui/SyncButlerUI/ErrorList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SyncButlerUI
+{
+	/// <summary>
+	/// Interaction logic for ErrorList.xaml
+	/// </summary>
+	public partial class ErrorList : Window
+	{
+		public ErrorList()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+        private void closeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            errorTable.RowGroups[1].Rows.Clear();
+            this.Hide();
+        }
+	}
+}
